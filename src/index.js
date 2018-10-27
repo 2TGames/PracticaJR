31be0a76bec019427675e34bf32fa3fb6807baa5
LuisBH98 <43405297+LuisBH98@users.noreply.github.com>
2018-10-27 14:05:45 +0200

    Recorrido entre pantalla inicio y menu. Tambien se han añadido el paso de un nivel a otro

--- src/index.js
+++ src/index.js
@@ -8,8 +8,10 @@ var game = new Phaser.Game(800, 590, Phaser.AUTO, 'gameDiv' );
 
 game.state.add('bootState', MagicAndRunes.bootState)
 game.state.add('preloadState', MagicAndRunes.preloadState)
+game.state.add('pantalla_cargaState', MagicAndRunes.pantalla_cargaState)
 game.state.add('menuState', MagicAndRunes.menuState)
 game.state.add('levelState', MagicAndRunes.levelState)
+game.state.add('level_izq1State', MagicAndRunes.level_izq1State)
 game.state.add('endingState', MagicAndRunes.endingState)
 
 
--- src/states/menu.js
+++ src/states/menu.js
@@ -1,39 +1,20 @@
 MagicAndRunes.menuState = function(game) {
 
 }
-var play;
-var nivel;
-var layer;
+
 MagicAndRunes.menuState.prototype = {
 
     preload: function() {
-        //game.load.image('background','assets/images/background_dungeon.png');
-        //game.load.tilemap('nivel0','assets/tiles/nivel0.csv',null,Phaser.Tilemap.CSV);
         
-        //game.load.image('tiles','assets/tiles/Tilesheet/medieval_tilesheet.png');
-        /*game.load.image('mago_izquierda','assets/images/mago_perfil_izq.png');
-        game.load.image('mago_derecha','assets/images/mago_perfil_derecho.png');
-        game.load.image('spell','assets/images/black_basic.png');*/
-        //game.load.spritesheet('mago','assets/images/caminar.png',30,50);
-        game.load.image('inicio','assets/images/pantalla_inicio.png');
+        game.load.image('fondo','assets/images/fondo magico.png');
     },
 
     create: function() {
-        
-        /*nivel=game.add.tilemap('nivel0',70,70);
-        
-        nivel.addTilesetImage('tiles');
-        console.log("Error");
-        layer= nivel.createLayer(0);
-        
-        layer.resizeWorld();
-*/        
-        /*var background= game.add.sprite(0,0,'background');
-        var mago_izquierda=game.add.sprite(100, 400,'mago_izquierda');
-        var mago_derecha=game.add.sprite(700,400,'mago_derecha');*/
-        var pantalla_inicio=game.add.image(0,0,'inicio');
-        var texto = game.add.text(250,500,"Press SPACE to play",{font:"30px Arial", fill:"#FFE400",align:"center"});
-        this.play=game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        var fondo=game.add.image(0,0,'fondo');
+        var texto = game.add.text(300,250,"1 Para jugar",{font:"30px Arial", fill:"#FFE400",align:"center"});
+        var texto2 = game.add.text(300,300,"2 Controles",{font:"30px Arial", fill:"#FFE400",align:"center"});
+        var texto = game.add.text(300,350,"3 Códice",{font:"30px Arial", fill:"#FFE400",align:"center"});
+        this.play=game.input.keyboard.addKey(Phaser.Keyboard.ONE);
     },
 
     update: function() {
