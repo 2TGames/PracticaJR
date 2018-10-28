e82a5640596271ca35cd62f39f9a657ff9a87d7d
LuisBH98 <43405297+LuisBH98@users.noreply.github.com>
2018-10-28 18:14:55 +0100

    Capturas de pantalla del juego

--- src/states/menu.js
+++ src/states/menu.js
@@ -12,8 +12,8 @@ MagicAndRunes.menuState.prototype = {
     create: function() {
         var fondo=game.add.image(0,0,'fondo');
         var texto = game.add.text(300,250,"1 Para jugar",{font:"30px Arial", fill:"#FFE400",align:"center"});
-        var texto2 = game.add.text(300,300,"2 Controles",{font:"30px Arial", fill:"#FFE400",align:"center"});
-        var texto = game.add.text(300,350,"3 Códice",{font:"30px Arial", fill:"#FFE400",align:"center"});
+        /*var texto2 = game.add.text(300,300,"2 Controles",{font:"30px Arial", fill:"#FFE400",align:"center"});
+        var texto = game.add.text(300,350,"3 Códice",{font:"30px Arial", fill:"#FFE400",align:"center"});*/
         this.play=game.input.keyboard.addKey(Phaser.Keyboard.ONE);
     },
 
