e7c07d57e9e2bf564ec0f3763deb3bc245b38dd9
LuisBH98 <43405297+LuisBH98@users.noreply.github.com>
2018-12-05 12:54:19 +0100

    arreglado websocket

--- Magic and Runes/src/main/java/es/urjc/jer/game/Application.java
+++ Magic and Runes/src/main/java/es/urjc/jer/game/Application.java
@@ -3,7 +3,6 @@ package es.urjc.jer.game;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
